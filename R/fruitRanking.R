1056a412f79571094ad0fbe9c0b934826a929bb7
Willdata <aiien61will@gmail.com>
2016-11-27 23:36:00 +0800

    update

--- R/fruitRanking.R
+++ R/fruitRanking.R
@@ -39,19 +39,20 @@ grading <- function(nu.df, rda.df, priceTable)
   }
   
   ### name variables
-  nu.factor <- c("維生素A效力(2)(RE)-成分值(ug)", "維生素E效力(α-TE)-成分值(mg)", "維生素C-成分值(mg)",           
-                 "維生素B1-成分值(mg)", "維生素B2-成分值(mg)", "菸鹼素-成分值(mg)",            
-                 "維生素B6-成分值(mg)", "葉酸-成分值(ug)",              
-                 "鈣-成分值(mg)", "磷-成分值(mg)", "鎂-成分值(mg)",                
-                 "鐵-成分值(mg)", "鋅-成分值(mg)",  "水分-成分值(g)")
+  nu.factor <- c("水分-成分值(g)", "維生素A效力(2)(RE)-成分值(ug)", "維生素E效力(α-TE)-成分值(mg)",           
+                 "維生素C-成分值(mg)", "維生素B1-成分值(mg)", "維生素B2-成分值(mg)", "菸鹼素-成分值(mg)",            
+                 "維生素B6-成分值(mg)", "葉酸-成分值(ug)", "鈣-成分值(mg)", "磷-成分值(mg)", 
+                 "鎂-成分值(mg)", "鐵-成分值(mg)", "鋅-成分值(mg)" )
   
   ### give each weighted value
-  weights <- c(rep(0.1, 8), rep(0.02, 5))
+  weights <- c(rep(0.1, 9), rep(0.02, 5))
   
   ### calculate every single fruit's each nutrition percentage
-  percentageTable <- apply(nu.df[,2:14], 1, function(x){ return(x/rda.df[,4:16])}) %>% 
+  percentageTable <- apply(nu.df[,3:15], 1, function(x){ return(x/rda.df[,4:16])}) %>% 
     data.table::rbindlist(.) %>% 
-    magrittr::set_rownames(., nu.df[, "作物名稱" ])
+    magrittr::set_rownames(., nu.df[, "水果名稱" ])
+  
+  percentageTable[, "水分-成分值(g)" := nu.df[, 2]]
   
   ### summary
   desc <- psych::describe(percentageTable)
@@ -60,21 +61,22 @@ grading <- function(nu.df, rda.df, priceTable)
   scoreMat <- emptyDF_Builder()
   
   ### adjust each fruit's nutrition scores
-  for(i in nu.factor[1:13]){
+  for(i in nu.factor[1:14]){
     scoreMat[1:18, i] <- scale(percentageTable[, i, with = FALSE], center = desc[i, "mean"], scale = desc[i, "sd"] )
   }
   
   ### define rownames
-  scoreMat <- magrittr::set_rownames(scoreMat, nu.df[, "作物名稱" ])
+  scoreMat <- magrittr::set_rownames(scoreMat, nu.df[, "水果名稱" ])
   
   ### calculate ranking score
-  scores.today <- apply(scoreMat[, 1:13], 1, function(x){ return( sum(x * weights)) } ) %>% 
-    magrittr::divide_by(., priceTable[, "price"]) %>% 
-    magrittr::subtract(min(.)) %>% 
-    magrittr::divide_by(., sum(.)) %>% 
-    magrittr::multiply_by(100) %>% 
+  scores.today <- apply(scoreMat[, 1:13], 1, function(x){ return( sum(x * weights * 10)) } ) %>% 
+    magrittr::divide_by(., priceTable[, "price"]) %>%  # for a fruit : total nutrition per 1 kg / today price per kg
+    magrittr::subtract(min(.)) %>%  # translation for no negative value shown for in case of misunderstanding
+    magrittr::divide_by(., sum(.)) %>%   # divided by sum of each variable for computing percentage
+    magrittr::multiply_by(100) %>%  # computing percentage
     sort(., decreasing = TRUE) %>% 
-    as.data.frame(.) %>% 
+    magrittr::add(., 1) %>%  # set min score = 1 
+    as.data.frame(.) %>%    
     magrittr::set_colnames(., "Ranking Score")
     
   return(scores.today)
