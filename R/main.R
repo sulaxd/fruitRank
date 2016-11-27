0cf53172e45b94d62f45a11c6f3599de65c08d81
Administrator <admin@example.com>
2016-11-27 12:55:00 +0800

    add two functions about data

--- R/main.R
+++ R/main.R
@@ -8,3 +8,19 @@ runApp <- function() {
 
     shiny::runApp(appDir, display.mode = "normal")
 }
+
+#' @export
+nutritDataUpdate <- function(){
+    require(rvest)
+    require(magrittr)
+    nutritDataUrl <- read_html("https://consumer.fda.gov.tw/Food/TFND.aspx?nodeID=178#")
+    nutritDataUrl <- nutritDataUrl %>% html_nodes("table.margintop.zoomBlock a") %>%
+        .[length(.)] %>% html_attr("href") %>% gsub("\\.\\.\\/","",.) %>%
+        paste0("https://consumer.fda.gov.tw/", .)
+    download.file(url = nutritDataUrl, destfile = "data/nutrition.xls", mode="wb")
+}
+
+#' @export
+getFruitTransData <- function(){
+    tmp2 <- RJSONIO::fromJSON('http://m.coa.gov.tw/OpenData/FarmTransData.aspx') %>% as.data.frame() %>% t()
+}
