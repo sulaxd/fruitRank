8bd2b67637356c7f7b2f681d429ba5b43a9ccada
Liu Ming Yu(Deron) <dsdeli8@gmail.com>
2016-12-03 23:33:11 +0800

    Update server.R

--- inst/shiny/myapp/server.R
+++ inst/shiny/myapp/server.R
@@ -1,15 +1,23 @@
 server <- function(input, output) {
-  
-  # Filter data based on selections
-  output$table <- DT::renderDataTable(DT::datatable({
-    data <- fruit
-    if (input$市場 != "所有市場") {
-      data <- data[data$市場 == input$市場,]
-    }
-    if (input$產品 != "所有產品") {
-      data <- data[data$產品 == input$產品,]
-    }
-    data
+  output$mytable1 <- DT::renderDataTable(DT::datatable({
+  data <- fruit
+  if (input$market != "所有市場") {
+  data <- data[data$市場 == input$market,]
+  }
+  if (input$products != "所有產品") {
+  data <- data[data$產品 == input$products,]
+  }
+  data
   }))
-
+  output$mytable2 <- DT::renderDataTable(DT::datatable({
+  data2 <- nutrition
+  if (input$classification != "所有食品") {
+    data2 <- data2[data2$食品分類 == input$classification,]
+  }
+  if (input$names != "所有樣品名稱") {
+    data2 <- data2[data2$樣品名稱 == input$names,]
+  }
+  data2
+  }))
+  
 }
