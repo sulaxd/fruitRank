79325633e53409038bd021c1cc7f93e0faa33be5
Liu Ming Yu(Deron) <dsdeli8@gmail.com>
2016-12-04 15:19:37 +0800

    Update ui.R

--- inst/shiny/myapp/ui.R
+++ inst/shiny/myapp/ui.R
@@ -1,14 +1,20 @@
+fruit <- getFruitTransData()
+nutrition <- read.csv("nutrition.csv",header = TRUE,stringsAsFactors = FALSE)
 ui <- fluidPage(
   titlePanel('Fruit Data'), # Give a title
   sidebarLayout(
     sidebarPanel(
       conditionalPanel(
+        'input.dataset === "Fruit Ranking"',
+        helpText('Fruit Ranking Select')
+      ),
+      conditionalPanel(
         'input.dataset === "Fruit Daily Trade Market"',
-        selectInput('market', '所有市場:',
-          c("所有市場", unique(as.character(fruit$市場名稱)))),
+        selectInput('products', '所有作物:',
+          c("所有作物", unique(as.character(fruit$作物名稱)))),
         hr(),
-        selectInput('products', '所有產品:',
-          c("所有產品", unique(as.character(fruit$作物名稱))))
+        selectInput('markets', '所有市場:',
+          c("所有市場", unique(as.character(fruit$市場名稱))))
       ),
       conditionalPanel(
         'input.dataset === "Fruit Nutrition"',
@@ -17,18 +23,14 @@ ui <- fluidPage(
         hr(),
         selectInput('names', '所有樣品名稱:',
                     c("所有樣品名稱", unique(as.character(nutrition$樣品名稱))))
-      ),
-      conditionalPanel(
-        'input.dataset === "Fruit Ranking"',
-        helpText('Fruit Ranking Seclect.')
       )
     ),
     mainPanel(
       tabsetPanel(
         id = 'dataset',
-        tabPanel('Fruit Daily Trade Market', DT::dataTableOutput('mytable1')),
-        tabPanel('Fruit Nutrition', DT::dataTableOutput('mytable2')),
-        tabPanel('Fruit Ranking', DT::dataTableOutput('mytable3'))
+        tabPanel('Fruit Ranking', DT::dataTableOutput('mytable1')),
+        tabPanel('Fruit Daily Trade Market', DT::dataTableOutput('mytable2')),
+        tabPanel('Fruit Nutrition', DT::dataTableOutput('mytable3'))
       )
     )
   )
